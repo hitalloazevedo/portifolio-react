8ba34f0cd81ef370d929554cd2dc78daeb53ba51
HitalloAzevedo <azevedohitallo@gmail.com>
2024-02-16 22:37:50 -0300

    feat: dynamic slider component and add some projects to carroussel

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,6 +3,8 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { Card } from './Card';
+
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -12,22 +14,49 @@ import '../assets/css/Slider.css'
 import '../assets/css/Media-Slider.css'
 import { useEffect, useState } from 'react';
 
+const projects = [
+  {
+    title: "Newsletter Landing Page",
+    description: "A landing page to subscribe in a newsletter.",
+    githubLink: "https://github.com/hitalloazevedo/newsletter-landing-page",
+    projectLink: "https://hitalloazevedo.github.io/newsletter-landing-page/",
+    imageUri: "https://i.pinimg.com/736x/4e/17/ae/4e17ae5487abe07b8b79ac17010edf83.jpg"
+},
+{
+    title: "Login Page Dark Theme",
+    description: "A simple login page built in dark theme.",
+    githubLink: "https://github.com/hitalloazevedo/tela-login-dark",
+    projectLink: "https://hitalloazevedo.github.io/tela-login-dark/",
+    imageUri: "https://i.pinimg.com/736x/1d/ad/8c/1dad8c62740644b5c4681bb12d81d184.jpg"
+},
+{
+    title: "Gym Landing Page",
+    description: "A landpage designed for gym owner.",
+    githubLink: "https://github.com/hitalloazevedo/lp-academia",
+    projectLink: "https://hitalloazevedo.github.io/landing-page-academia/",
+    imageUri: "https://i.pinimg.com/736x/cb/54/32/cb543250be8d485a210db4802120e6e4.jpg"
+},
+]
+
 const Slider = () => {
 
   const [width, setWidth] = useState(window.innerWidth)
   const [slidesPerView, setSlidesPerView] = useState(1)
 
   useEffect(() => {
+    window.addEventListener('DOMContentLoaded', () =>{
+      setWidth(window.innerWidth)
+    })
     window.addEventListener('resize', () => {
       setWidth(window.innerWidth)
       
       if (width < 768) {
         setSlidesPerView(1)
       }
-      if (width > 768 && width < 1024) {
+      if (width > 768 && width < 1230) {
         setSlidesPerView(2)
       } 
-      if (width > 1025) {
+      if (width > 1230) {
         setSlidesPerView(3)
       }
 
@@ -42,18 +71,12 @@ const Slider = () => {
     navigation
     pagination={{ clickable: true }}
     scrollbar={{ draggable: true }}
-    onSwiper={(swiper) => console.log(swiper)}
-    onSlideChange={() => console.log('slide change')}
+    onSwiper={(swiper) => ''}
+    onSlideChange={() => ''}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      {projects.map((project) => (
+        <SwiperSlide><Card title={project.title} description={project.description} imageUri={project.imageUri} githubLink={project.githubLink} projectLink={project.projectLink}/></SwiperSlide>
+      ))}
       ...
     </Swiper>
   );
